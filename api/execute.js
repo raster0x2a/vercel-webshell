8b94f61651d7db365427925e6704da61cd8414f4
R* <64038390+raster0x2a@users.noreply.github.com>
2025-07-26 00:37:45 +0900

    Update execute.js

--- api/execute.js
+++ api/execute.js
@@ -4,10 +4,6 @@ const crypto = require('crypto');
 
 const SECRET_HASH = "4be7de85c6bad019834ef6843189edd6a5710e800806794349658fcafdc83af9";
 
-// 実行を許可する安全なコマンドのホワイトリスト
-const ALLOWED_COMMANDS = new Set([
-]);
-
 export default function handler(req, res) {
   // pass check
   const hash = crypto.createHash('sha256');
@@ -30,11 +26,6 @@ export default function handler(req, res) {
 
   // コマンドと引数を分割
   const [cmd, ...args] = command.trim().split(/\s+/);
-
-  // ホワイトリストに含まれているかチェック
-  if (!ALLOWED_COMMANDS.has(cmd)) {
-    return res.status(403).json({ output: `Error: Command not allowed: ${cmd}` });
-  }
   
   // VercelのServerless Functionは書き込み不可のファイルシステムなので、
   // 'ls -F' のような引数は安全と見なせる
